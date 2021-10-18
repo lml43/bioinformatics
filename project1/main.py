9d1fb28e535cc2f3efd9ef59f159f127e8931c00
manhlinh0403 <manhlinh0403@gmail.com>
2021-10-18 22:20:47 +0200

    generate alignment

--- project1/main.py
+++ project1/main.py
@@ -1,5 +1,4 @@
 
-
 GAP_PENALTY = -2
 SAME_AWARD = 2
 DIFFERENCE_PENALTY = -1
@@ -9,6 +8,7 @@ gen1 = 'ACCAGG'
 gen2 = 'GACCA'
 # gen1 = 'CACGA'
 # gen2 = 'CGA'
+
 
 class Node:
     def __init__(self):
@@ -28,7 +28,7 @@ def maximum(a, b, c):
     return max(list)
 
 
-def recreateAlignment(gen1, gen2):
+def createScoreMatrix(gen1, gen2):
     col = len(gen2) + 1
     row = len(gen1) + 1
     
@@ -36,37 +36,67 @@ def recreateAlignment(gen1, gen2):
 
     for i in range(1, row):
         mat[i][0].score = mat[i-1][0].score + GAP_PENALTY
+        mat[i][0].origins.append(('up', (i-1, 0)))
 
     for j in range(1, col):
         mat[0][j].score += mat[0][j-1].score + GAP_PENALTY
+        mat[0][j].origins.append(('left', (0, j-1)))
 
     for i in range(1, row):
         for j in  range(1, col):
-            if gen1[i-1] == gen2[j-1]:
-                diagonal = mat[i-1][j-1].score + SAME_AWARD
+            if gen1[ i -1] == gen2[ j -1]:
+                diagonal = mat[ i -1][ j -1].score + SAME_AWARD
             else:
-                diagonal = mat[i-1][j-1].score + DIFFERENCE_PENALTY
+                diagonal = mat[ i -1][ j -1].score + DIFFERENCE_PENALTY
 
-            up = mat[i-1][j].score + GAP_PENALTY
-            left = mat[i][j-1].score + GAP_PENALTY
+            up = mat[ i -1][j].score + GAP_PENALTY
+            left = mat[i][ j -1].score + GAP_PENALTY
 
             max = maximum(up, left, diagonal)
             arrows = []
 
             if up == max:
-                arrows.append((i-1, j))
+                arrows.append(("up", ( i -1, j)))
 
             if left == max:
-                arrows.append((i, j-1))
+                arrows.append(("left", (i, j- 1)))
 
             if diagonal == max:
-                arrows.append((i-1, j-1))
+                arrows.append(("diagonal", (i - 1, j - 1)))
 
             mat[i][j].score = max
             mat[i][j].origins = arrows
 
-    printMat(mat)
+    return mat
 
 
+lst = []
 
-recreateAlignment(gen2, gen1)
+
+def generateAlignments(mat, gen1, gen2, currentGen1, currentGen2, currentI, currentJ):
+    if currentI == 0 and currentJ == 0:
+        lst.append((currentGen1, currentGen2))
+        return
+
+    for direction, coordinate in mat[currentI][currentJ].origins:
+        newGen1 = currentGen1
+        newGen2 = currentGen2
+
+        if direction == 'diagonal':
+            newGen1 = gen1[currentI-1] + currentGen1
+            newGen2 = gen2[currentJ-1] + currentGen2
+
+        if direction == 'left':
+            newGen1 = '_' + currentGen1
+            newGen2 = gen2[currentJ-1] + currentGen2
+
+        if direction == 'up':
+            newGen1 = gen1[currentI-1] + currentGen1
+            newGen2 = '_' + currentGen2
+
+        generateAlignments(mat, gen1, gen2, newGen1, newGen2, coordinate[0], coordinate[1])
+
+
+mat = createScoreMatrix(gen2, gen1)
+generateAlignments(mat, gen2, gen1, '', '', len(gen2), len(gen1))
+print(lst)
