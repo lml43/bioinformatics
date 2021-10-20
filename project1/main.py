d1e52b4370209e6c1d1dbda2e05b65abc0a0411f
manhlinh0403 <manhlinh0403@gmail.com>
2021-10-20 08:43:35 +0200

    read genes from files

--- project1/main.py
+++ project1/main.py
@@ -1,11 +1,26 @@
-
 GAP_PENALTY = -2
 SAME_AWARD = 2
 DIFFERENCE_PENALTY = -1
 MAX_SEQ_LENGTH = 1000
 
-gen1 = 'ACCAGG'
-gen2 = 'GACCA'
+
+def readGeneFromFile(file_name):
+    f = open(file_name)
+    input = f.read()
+    lst = input.split('\n')
+    gene = ''
+    for str in lst:
+        if str[0] != '>':
+            gene = gene + str
+    return gene
+
+
+gen1 = readGeneFromFile("./data/protein1.fasta")
+gen2 = readGeneFromFile("./data/protein2.fasta")
+
+
+# gen1 = 'ACCAGG'
+# gen2 = 'GACCA'
 # gen1 = 'CACGA'
 # gen2 = 'CGA'
 
@@ -16,61 +31,67 @@ class Node:
         self.origins = []
 
 
-def printMat(mat):
-    for i in range(len(mat)):
-        for j in range(len(mat[i])):
-            print(mat[i][j].score, end="\t")
+def printMat(matToPrint):
+    for i in range(len(matToPrint)):
+        for j in range(len(matToPrint[i])):
+            print(matToPrint[i][j].score, end="\t")
         print()
 
 
 def maximum(a, b, c):
-    list = [a, b, c]
-    return max(list)
+    list_el = [a, b, c]
+    return max(list_el)
+
+
+def initMatrix(row, col):
+    mat = [[Node() for c in range(col)] for r in range(row)]
+    for i in range(1, row):
+        mat[i][0].score = mat[i - 1][0].score + GAP_PENALTY
+        mat[i][0].origins.append(('up', (i - 1, 0)))
+    for j in range(1, col):
+        mat[0][j].score += mat[0][j - 1].score + GAP_PENALTY
+        mat[0][j].origins.append(('left', (0, j - 1)))
+    return mat
+
+
+def generateArrows(i, j, diagonal, left, up, max):
+    arrows = []
+    if up == max:
+        arrows.append(("up", (i - 1, j)))
+    if left == max:
+        arrows.append(("left", (i, j - 1)))
+    if diagonal == max:
+        arrows.append(("diagonal", (i - 1, j - 1)))
+    return arrows
+
+
+def calculateScores(gen1, gen2, i, j, mat):
+    if gen1[i - 1] == gen2[j - 1]:
+        diagonal = mat[i - 1][j - 1].score + SAME_AWARD
+    else:
+        diagonal = mat[i - 1][j - 1].score + DIFFERENCE_PENALTY
+    up = mat[i - 1][j].score + GAP_PENALTY
+    left = mat[i][j - 1].score + GAP_PENALTY
+    return diagonal, left, up
 
 
 def createScoreMatrix(gen1, gen2):
     col = len(gen2) + 1
     row = len(gen1) + 1
-    
-    mat = [[Node() for c in range(col)] for r in range(row)]
+
+    matrix = initMatrix(row, col)
 
     for i in range(1, row):
-        mat[i][0].score = mat[i-1][0].score + GAP_PENALTY
-        mat[i][0].origins.append(('up', (i-1, 0)))
+        for j in range(1, col):
+            diagonal, left, up = calculateScores(gen1, gen2, i, j, matrix)
 
-    for j in range(1, col):
-        mat[0][j].score += mat[0][j-1].score + GAP_PENALTY
-        mat[0][j].origins.append(('left', (0, j-1)))
+            max_score = maximum(up, left, diagonal)
+            arrows = generateArrows(i, j, diagonal, left, up, max_score)
 
-    for i in range(1, row):
-        for j in  range(1, col):
-            if gen1[ i -1] == gen2[ j -1]:
-                diagonal = mat[ i -1][ j -1].score + SAME_AWARD
-            else:
-                diagonal = mat[ i -1][ j -1].score + DIFFERENCE_PENALTY
+            matrix[i][j].score = max_score
+            matrix[i][j].origins = arrows
 
-            up = mat[ i -1][j].score + GAP_PENALTY
-            left = mat[i][ j -1].score + GAP_PENALTY
-
-            max = maximum(up, left, diagonal)
-            arrows = []
-
-            if up == max:
-                arrows.append(("up", ( i -1, j)))
-
-            if left == max:
-                arrows.append(("left", (i, j- 1)))
-
-            if diagonal == max:
-                arrows.append(("diagonal", (i - 1, j - 1)))
-
-            mat[i][j].score = max
-            mat[i][j].origins = arrows
-
-    return mat
-
-
-lst = []
+    return matrix
 
 
 def generateAlignments(mat, gen1, gen2, currentGen1, currentGen2, currentI, currentJ):
@@ -83,20 +104,22 @@ def generateAlignments(mat, gen1, gen2, currentGen1, currentGen2, currentI, curr
         newGen2 = currentGen2
 
         if direction == 'diagonal':
-            newGen1 = gen1[currentI-1] + currentGen1
-            newGen2 = gen2[currentJ-1] + currentGen2
+            newGen1 = gen1[currentI - 1] + currentGen1
+            newGen2 = gen2[currentJ - 1] + currentGen2
 
         if direction == 'left':
             newGen1 = '_' + currentGen1
-            newGen2 = gen2[currentJ-1] + currentGen2
+            newGen2 = gen2[currentJ - 1] + currentGen2
 
         if direction == 'up':
-            newGen1 = gen1[currentI-1] + currentGen1
+            newGen1 = gen1[currentI - 1] + currentGen1
             newGen2 = '_' + currentGen2
 
         generateAlignments(mat, gen1, gen2, newGen1, newGen2, coordinate[0], coordinate[1])
 
 
+lst = []
 mat = createScoreMatrix(gen2, gen1)
+# print(mat[len(gen2)][len(gen1)].score)
 generateAlignments(mat, gen2, gen1, '', '', len(gen2), len(gen1))
-print(lst)
+print(len(lst))
